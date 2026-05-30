e8aebcc5f53fe526cfae6d5a3f9f1cbae296e4ef
Kingcode71 <2425400813@live.gctu.edu.gh>
2026-05-30 08:17:27 -0700

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+#File: initials.py
+#Description: Python codes to print my initials in large letters
+#Assignment Number: 2
+#Name: Kingsford Adu Ampomah
+#Student ID: 2425400813
+#Email: 2425400813@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#On my honour, Kingsford Adu Ampomah, this programming assignment is my own work
+#and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+#Codes to print out my initials in large letters
 
+    print()
+    print('...KAA')
+    print()
 
+    print('...KK........KK...........AAAAAA..............AAAAAA........')
+    print('...KK......KK...........AA......AA..........AA......AA......')
+    print('...KK.....KK...........AA........AA........AA........AA.....')
+    print('...KK....KK............AA........AA........AA........AA.....')
+    print('...KK..KK..............AA........AA........AA........AA.....')
+    print('...KKKK................AAAAAAAAAAAA........AAAAAAAAAAAA.....')
+    print('...KK..KK..............AA........AA........AA........AA.....')
+    print('...KK....KK............AA........AA........AA........AA.....')
+    print('...KK......KK.....**...AA........AA...**...AA........AA...**')
+    print('...KK........KK...**...AA........AA...**...AA........AA...**')
+
+    print()
 
 main()
